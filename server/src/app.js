5e50ee86ea012d04f14b3d2cf35cd3eab1123659
AGpeter2018 <adenijipeter2018@gmail.com>
2026-01-20 12:44:29 +0100

    fix new cors issue

--- server/src/app.js
+++ server/src/app.js
@@ -23,7 +23,7 @@ app.use(express.json())
 const allowedOrigins = [
   "http://localhost:5173",
   "http://127.0.0.1:5173",
-  env.process.CLIENT_URL 
+  process.env.CLIENT_URL 
 ];
 
 app.use(cors({
